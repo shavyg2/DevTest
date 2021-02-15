1381515932e80c955fdd718d9b227cc18d5fdbaa
Steven Le <steven87.le@gmail.com>
2021-02-15 16:48:01 -0500

    DoTest-Q1: refactor fuctions in Q3

--- question3/main.ts
+++ question3/main.ts
@@ -3,51 +3,22 @@ export class Connect4 {
     constructor() {
       this._isNewGameStart = true;
       this._isGameFinished = false;
-      this._movingRecord["player1"] = true; 
+      this._player1  = new Player1();
+      this._player2  = new Player2();
+      this._currentPlayer = this._player1;
     }
   
     play(col: number): string{
+      var messageRes: string;
       if(this._isGameFinished) {
-        this._message = InstructionMessage.GameFinished;
-        return this._message;
+        messageRes = InstructionMessage.GameFinished;
+        return messageRes;
       }
-      if(this._movingRecord["player1"] == true){
-        this._message = InstructionMessage.Player1Turn;
-        let row = this._findNextRow(col);
-        if(row == -1) {
-          this._message = InstructionMessage.ColumnFull;
-          return this._message;
-        }
-        this._board[row][col] = 1; 
-        let isWin = this._checkWinner();
-        if(isWin) this._message = "Player 1 wins!";
-        else{
-          this._movingRecord["player1"] = false;
-          this._movingRecord["player2"] = true;
-        }
-        
-      } 
-      else if (this._movingRecord["player2"] == true){
-        this._message = InstructionMessage.Player2Turn;
-        let row = this._findNextRow(col);
-        if(row == -1) {
-          this._message = InstructionMessage.ColumnFull;
-          return;
-        }
-        this._board[row][col] = 2; 
-        let isWin = this._checkWinner();
-        if(isWin) this._message = "Player 2 wins!";
-        else{
-          this._movingRecord["player1"] = true;
-          this._movingRecord["player2"] = false;
-        }
-      } 
-
-      //After playing moved:
-      return this._message;
+      messageRes =  this._playerMove(this._currentPlayer, col);
+      return messageRes;
     }
 
-    //area of private params
+    //field of private params
     _board= [ [0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0],
@@ -56,10 +27,33 @@ export class Connect4 {
               [0,0,0,0,0,0,0]
             ];
 
-    _movingRecord = {"player1": true, "player2": false};
     _isNewGameStart: boolean ;
     _isGameFinished: boolean ;
-    _message: string;
+    _player1 : PlayerInfo;
+    _player2: PlayerInfo;
+    _currentPlayer: PlayerInfo;
+
+    _playerMove(player: PlayerInfo, col: number): string{
+      let message : string;
+      message = player.PlayerTurnMessage;
+      let row = this._findNextRow(col);
+      if(row == -1) {
+        message = InstructionMessage.ColumnFull;
+        return message;
+      }
+      this._board[row][col] = player.PlayerMoveSign;
+      let isWin = this._checkWinner();
+      if(isWin) {
+        message = player.PlayerWinMessage;
+      }
+      else
+      {
+        if( player instanceof Player1 ) this._currentPlayer = this._player2;
+        else if ( player instanceof Player2 ) this._currentPlayer = this._player1;
+        this._currentPlayer.ShouldPlayerMove = true;
+      }
+      return message;
+    }
 
     _checkWinner() : boolean{
       let isWin = (this._checkAllRowSame() || this._checkAllColumnSame() || this._checkAllDiagonalSame());
@@ -97,7 +91,6 @@ export class Connect4 {
     }
 
     _checkAllColumnSame() : boolean {
-      console.log(this._board);
       let count = 0;
       for (let i = 0; i < 6; i++) {
           var flag : Boolean = true;
@@ -116,7 +109,6 @@ export class Connect4 {
     }
 
     _checkAllDiagonalSame() : boolean {
-     
       let count = 0;
       for (let k = 0; k<=2; k++) {
         let j = 0;
@@ -181,16 +173,39 @@ export class Connect4 {
           j++;
         }
       }
-      
       return false;
     }
-    
-
   }
 
-  enum InstructionMessage{
+  enum InstructionMessage {
     "Player1Turn" = "Player 1 has a turn",
     "Player2Turn" = "Player 2 has a turn",
+    "Player1Win" = "Player 1 wins!",
+    "Player2Win" = "Player 2 wins!",
     "ColumnFull"  = "Column full!",
     "GameFinished" = "Game has finished!"
+  }
+
+ interface PlayerInfo {
+    PlayerName: string ;
+    PlayerTurnMessage: string ;
+    PlayerWinMessage: string ;
+    ShouldPlayerMove: boolean ;
+    PlayerMoveSign: number;
+  }
+
+  class Player1 implements PlayerInfo{
+    PlayerName = "Player1";
+    PlayerTurnMessage = InstructionMessage.Player1Turn;
+    PlayerWinMessage = InstructionMessage.Player1Win;
+    PlayerMoveSign = 1;
+    ShouldPlayerMove = true ;
+  }
+
+  class Player2 implements PlayerInfo{
+    PlayerName = "Player2";
+    PlayerTurnMessage = InstructionMessage.Player2Turn;
+    PlayerWinMessage = InstructionMessage.Player2Win;
+    PlayerMoveSign = 2;
+    ShouldPlayerMove = false ;
   }
