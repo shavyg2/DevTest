efce8ccde0c8082b2b834ed05463934c3c2101bb
DharaGelani <79169035+DharaGelani@users.noreply.github.com>
2021-02-16 15:26:40 -0500

    Update main.ts
    I have complete this task in C#.

--- question1/main.ts
+++ question1/main.ts
@@ -1,4 +1,37 @@
-export function isValidWalk(walk: string[]) {
+function isValidWalk(walk) {
   
-}
+  let person = [0, 0]
+  
+  let finalDestination = [0, 0]
+  let north = [0, 1]
+  let east = [1, 0]
+  let south = [0, -1]
+  let west = [-1, 0]
+ 
+  for (let i = 0; i <= 10; i++) {
+    
+    if (walk[i] === "n") {      
+      person[0]++;
+    }
+    
+    if (walk[i] === "e") { 
+      person[1]++;
+    }
+   
+    if (walk[i] === "s") { 
+       person[0]--;
+    }
+   
+    if (walk[i] === "w") { 
+      person[1]--;
+    }
+  }
 
+  if (person[0] === finalDestination[0] &&
+    person[1] === finalDestination[1]) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
