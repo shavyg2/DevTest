2a8dd27014071e3228e03001fbeccce81811de06
issayev <issayev@sheridancollege.ca>
2021-02-24 12:56:30 -0500

    Finished all 3 questions

--- question1/main.ts
+++ question1/main.ts
@@ -1,4 +1,26 @@
 export function isValidWalk(walk: string[]) {
-  
-}
+  let x = 0;
+  let y = 0;
 
+  if (walk.length == 10) {
+    for (let i = 0; i < walk.length; i++) {
+      if (walk[i] == "n") {
+        y++;
+      } else if (walk[i] == "s") {
+        y--;
+      } else if (walk[i] == "e") {
+        x++;
+      } else if (walk[i] == "w") {
+        x--;
+      }
+    }
+
+    if (x == 0 && y == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,11 @@
 export function findOutlier(integers: number[]): number {
-    
-  }
+  return integers.slice(0, 3).filter(even).length >= 2
+    ? integers.find(odd)
+    : integers.find(even);
+}
+function even(num) {
+  return num % 2 === 0;
+}
+function odd(num) {
+  return !even(num);
+}
